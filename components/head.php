0e78d5997ec646466153b04b8e3c219d712f69a3
Pengguna <nopinio70@gmail.com>
2026-07-15 10:36:15 +0800

    faile css tersusun

--- components/head.php
+++ components/head.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informasi Rasmi KVks</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/nav.css">
+    <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/component.css">
     <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/heroSection.css">
-    <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/landStyle.css">
-    <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/footer.css">
+    <link rel="stylesheet" href="<?= $baseUrl ?>asset/css/login.css">
     <style>
 html, body{
     scrollbar-width:none;
